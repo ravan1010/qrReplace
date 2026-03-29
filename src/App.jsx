39a73d04d16ea3cd86da3ea457ab8f51b8ad491c
ravan1010 <ravanravana177@gmail.com>
2026-03-29 23:01:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import QRCode from "qrcode";
+
+export default function App() {
+
+  const [qr,setQr] = useState("");
+
+  useEffect(()=>{
+
+    QRCode.toDataURL(
+      "upi://pay?pa=suhasnayaj-1@oksbi&pn=prabha%20(sbi%20Bank)&am=200&cu=INR"
+    ).then(setQr);
+
+  },[]);
+
+
+  const shareImage = async () => {
+
+    const res = await fetch(qr);
+    const blob = await res.blob();
+
+    const file = new File([blob], "upi-qr.png", {
+      type: "image/png"
+    });
+
+    if (navigator.share) {
+
+      await navigator.share({
+        title: "Pay via UPI",
+        text: "Scan and pay",
+        files: [file]
+      });
+
+    } else {
+      alert("Sharing not supported on this browser");
+    }
+
+  };
+
+  return(
+
+    <div style={{textAlign:"center"}}>
+
+      <h2>Scan & Pay</h2>
+
+      {qr && <img src={qr} alt="qr" width="200"/>}
+
+      <br/><br/>
+
+      <button onClick={shareImage}>
+        Share QR
+      </button>
+
+    </div>
+
+  );
+
+}
